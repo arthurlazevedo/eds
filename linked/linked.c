c37d98034e68cfc7b5d56f9953a72c4504e5428a
arthur.ledra.ext <arthur.ledra.ext@synchro.com.br>
2026-02-28 22:35:11 -0300

    refatora linked

--- linked/linked.c
+++ linked/linked.c
@@ -1,219 +1,47 @@
-#include <linked.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 #include <assert.h>
 
+#include "linked.h"
 
-static void set_head_tail(Linked_List* ll, Node* node)
-{
-    ll->head = node;
-    ll->tail = node;
-}
+typedef struct Node Node;
 
+struct Node {
+    Node  *prev;
+    Node  *next;
+    int    value;
+};
 
-bool ll_is_empty(Linked_List* ll)
-{
-    return ll->head == NULL;
-}
-
-
-void ll_add_first(Linked_List* ll, int value)
-{
-    Node* new_node = NULL;
-    set_node(new_node, value);
-    
-    if (ll_is_empty(ll)) {
-        set_head_tail(ll, new_node);
-    } else {
-        ll->head->prev = (struct Node*) new_node;
-        new_node->next = (struct Node*) ll->head;
-        ll->head = new_node;
-    }
-
-    ll->size++;
-}
-
-
-void ll_add_last(Linked_List* ll, int value)
-{
-    Node* new_node = NULL;
-    set_node(new_node, value);
-
-    if (ll_is_empty(ll)) {
-        set_head_tail(ll, new_node);
-    } else {
-        ll->tail->next = (struct Node*) new_node;
-        new_node->prev = (struct Node*) ll->tail;
-        ll->tail = new_node;
-    }
-
-    ll->size++;
-}
-
-
-bool ll_add(Linked_List* ll, int value, size_t index)
-{
-    if (index >= ll->size) return false;
-
-    if (index == 0 || ll_is_empty(ll)) {
-        ll_add_first(ll, value);        
-        return true;
-    }
-
-    Node* new_node = NULL;
-    set_node(new_node, value);
-
-    Node* aux = ll->head;
-    for (size_t i = 1; i < index; i++) aux = (Node*) aux->next;
-
-    new_node->next = aux->next;
-    new_node->prev = (struct Node*) aux;
-    aux->next = (struct Node*) new_node;
-    ((Node*) new_node->next)->prev = (struct Node*) new_node;
-
-    ll->size++;
-}
-
-
-bool ll_remove_first(Linked_List* ll)
-{
-    if (ll_is_empty(ll)) return false;
-
-    if (ll->size == 1) {
-        free(ll->head);
-        set_head_tail(ll, NULL);
-    } else {
-        ll->head = (Node*) ll->head->next;
-        free(ll->head->prev);
-        ll->head->prev = NULL;
-    }
-
-    ll->size--;
-    return true;
-}
-
-
-bool ll_remove_last(Linked_List* ll)
-{
-    if (ll_is_empty(ll)) return false;
-
-    if (ll->size == 1) {
-        free(ll->tail);
-        set_head_tail(ll, NULL);
-    } else {
-        ll->tail = (Node*) ll->tail->prev;
-        free(ll->tail->next);
-        ll->tail->next = NULL;
-    }
-
-    ll->size--;
-    return true;
-}
-
-
-bool ll_remove(Linked_List* ll, size_t index)
-{
-    if (ll_is_empty(ll) || index >= ll->size) return false;
-
-    if (index == 0) {
-        ll_remove_first(ll);
-        return true;
-    }
-
-    if (index == ll->size - 1) {
-        ll_remove_last(ll);
-        return true;
-    }
-
-    Node* aux = ll->head;
-    for (size_t i = 1; i <= index; i++) aux = (Node*) aux->next;
-
-    ((Node*) aux->prev)->next = aux->next;
-    ((Node*) aux->next)->prev = aux->prev;
-    free(aux);
-
-    ll->size--;
-    return true;
-}
-
-
-int* ll_get_first(Linked_List* ll)
-{
-    if (ll_is_empty(ll)) return NULL;
-
-    return &(ll->head->value);
-}
-
-
-int* ll_get_last(Linked_List* ll)
-{
-    if (ll_is_empty(ll)) return NULL;
-
-    return &(ll->tail->value);
-}
-
-
-int* ll_get(Linked_List* ll, size_t index)
-{
-    if (index >= ll->size) return NULL;
-    if (index == 0) return ll_get_first(ll);
-    if (index == ll->size - 1) return ll_get_last(ll);
-
-    Node* aux = ll->head;
-    for (size_t i = 1; i <= index; i++) aux = (Node*) aux->next;
-
-    return &(aux->value);
-}
-
-
-int ll_index_of(Linked_List* ll, int value)
-{
-    Node* aux = ll->head;
-    int idx = 0;
-
-    while (aux) {
-        if (aux->value == value) return idx;
-        aux = (Node*) aux->next;
-        idx++;
-    }
-    
-    return -1;
-}
-
-
-int ll_last_index_of(Linked_List* ll, int value)
-{
-    Node* aux = ll->tail;
-    int idx = ll->size - 1;
-
-    while (aux) {
-        if (aux->value == value) return idx;
-        aux = (Node*) aux->prev;
-        idx--;
-    }
-
-    return -1;
-}
-
-
-bool ll_contains(Linked_List* ll, int value)
-{
-    return ll_index_of(ll, value) != -1;
-}
+typedef struct {
+    size_t  size;
+    Node   *head;
+    Node   *tail;
+} Linked_List;
 
 
 int main(void)
 {
-    Linked_List ll = {0};
+    Linked_List linked = {0};
+    Linked_List *ll = &linked;
 
-    ll_add_first(&ll, 0);
-    assert(ll.size == 1);
-    assert(!ll_is_empty(&ll));
-    assert(*ll_get_first(&ll) == 0);
+    ll_add_first(&linked, 1);
+    assert(linked.size == 1);
+    assert(linked.head->value == 1);
 
-    ll_remove_first(&ll);
-    assert(ll.size == 0);
-    assert(ll_is_empty(&ll));
-    assert(ll_get_first(&ll) == NULL);
+    ll_remove_head(&linked);
+    assert(linked.size == 0);
+    assert(linked.head == NULL);
+
+    ll_add_last(&linked, 1);
+    assert(linked.size == 1);
+    assert(linked.head->value == 1);
+
+    ll_add_last(&linked, 2);
+    assert(linked.size == 2);
+    assert(linked.head->next->value == 2);
+
+    assert(ll_get(&linked, 1) == 2);
 
     return 0;
 }
--- linked/linked.h
+++ linked/linked.h
@@ -1,49 +1,149 @@
 #include <stdio.h>
+#include <assert.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct {
-    int value;
-    struct Node* prev;
-    struct Node* next;
-} Node;
 
-typedef struct {
-    size_t size;
-    Node* head;
-    Node* tail;
-} Linked_List;
-
-typedef struct {
-    char* data;
-    size_t len;
-    size_t cap;
-} String_Builder;
-
-#define set_node(node, value) \
-    do { \
-        (node) = malloc(sizeof(Node)); \
-        (node)->value = value; \
-    } while (0);
+#define set_head_tail(ll, node)                             \
+    do {                                                    \
+        (ll)->head = node;                                  \
+        (ll)->tail = node;                                  \
+    } while (0)
 
 
-bool (ll_is_empty)     (Linked_List*);
+#define set_node(node, val)                                 \
+    do {                                                    \
+        (node) = malloc(sizeof(*(node)));                   \
+        (node)->value = val;                                \
+    } while (0)
 
-void (ll_add_first)    (Linked_List*, int);
-void (ll_add_last)     (Linked_List*, int);
-bool (ll_add)          (Linked_List*, int, size_t);
 
-bool (ll_remove_first) (Linked_List*);
-bool (ll_remove_last)  (Linked_List*);
-bool (ll_remove)       (Linked_List*, size_t);
+#define ll_add_first(ll, val)                               \
+    do {                                                    \
+        typeof((ll)->head) new_node = NULL;                 \
+        set_node(new_node, val);                            \
+                                                            \
+        if ((ll)->size == 0) {                              \
+            set_head_tail(ll, new_node);                    \
+        } else {                                            \
+            (ll)->head->prev = new_node;                    \
+            new_node->next = (ll)->head;                    \
+            (ll)->head = new_node;                          \
+        }                                                   \
+                                                            \
+        (ll)->size++;                                       \
+    } while (0)
 
-int* (ll_get_first)    (Linked_List*);
-int* (ll_get_last)     (Linked_List*);
-int* (ll_get)          (Linked_List*, size_t);
 
-int (ll_index_of)      (Linked_List*, int);
-int (ll_last_index_of) (Linked_List*, int);
-bool (ll_contains)     (Linked_List*, int);
+#define ll_add_last(ll, val)                                \
+    do {                                                    \
+        typeof((ll)->head) new_node = NULL;                 \
+        set_node(new_node, val);                            \
+                                                            \
+        if ((ll)->size == 0) {                              \
+            set_head_tail((ll), new_node);                  \
+        } else {                                            \
+            (ll)->tail->next = new_node;                    \
+            new_node->prev = (ll)->tail;                    \
+            (ll)->tail = new_node;                          \
+        }                                                   \
+                                                            \
+        (ll)->size++;                                       \
+    } while (0)
 
-// TODO: to_string
-char* (ll_to_string)   (Linked_List*);
+
+#define ll_add(ll, val, index)                              \
+    do {                                                    \
+        if (index == 0 || (ll)->size == 0)                  \
+            ll_add_first(ll, val);                          \
+                                                            \
+        assert(index < (ll)->size);                         \
+                                                            \
+        typeof((ll)->head) new_node = NULL;                 \
+        set_node(new_node, (val));                          \
+                                                            \
+        typeof((ll)->head) aux = (ll)->head;                \
+        for (size_t i = 1; i < index; i++)                  \
+            aux = aux->next;                                \
+                                                            \
+        new_node->next = aux->next;                         \
+        new_node->prev = aux;                               \
+        aux->next = new_node;                               \
+        new_node->next->prev = new_node;                    \
+                                                            \
+        (ll)->size++;                                       \
+    } while (0)
+
+
+#define ll_remove_head(ll)                                  \
+    do {                                                    \
+        assert((ll)->size);                                 \
+                                                            \
+        if ((ll)->size == 1) {                              \
+            free((ll)->head);                               \
+            set_head_tail((ll), NULL);                      \
+        } else {                                            \
+            (ll)->head = (ll)->head->next;                  \
+            free((ll)->head->prev);                         \
+            (ll)->head->prev = NULL;                        \
+        }                                                   \
+                                                            \
+        (ll)->size--;                                       \
+    } while (0)
+
+
+#define ll_remove_tail(ll)                                  \
+    do {                                                    \
+        assert((ll)->size);                                 \
+                                                            \
+        if ((ll)->size == 1) {                              \
+            free((ll)->tail);                               \
+            set_head_tail(ll, NULL);                        \
+        } else {                                            \
+            (ll)->tail = (ll)->tail->prev;                  \
+            free((ll)->tail->next);                         \
+            (ll)->tail->next = NULL;                        \
+        }                                                   \
+                                                            \
+        (ll)->size--;                                       \
+    } while (0)
+
+
+#define ll_remove(ll, index)                                \
+    do {                                                    \
+        assert((ll)->size && index < (ll)->size);           \
+                                                            \
+        if ((index) == 0)                                   \
+            ll_remove_first(ll);                            \
+                                                            \
+        if ((index) == (ll)->size - 1)                      \
+            ll_remove_last(ll);                             \
+                                                            \
+        typeof((ll)->head) aux = (ll)->head;                \
+        for (size_t i = 1; i <= index; i++)                 \
+            aux = aux->next;                                \
+                                                            \
+        aux->prev->next = aux->next;                        \
+        aux->next->prev = aux->prev;                        \
+        free(aux);                                          \
+                                                            \
+        (ll)->size--;                                       \
+    } while (0)
+
+
+// o nome disso é statement expressions, muito útil
+#define ll_get(ll, index)                                   \
+    ({                                                      \
+        assert((ll)->size && index < (ll)->size);           \
+        typeof((ll)->head->value) res;                      \
+        if (index == (ll)->size - 1) {                      \
+            res = (ll)->tail->value;                        \
+        } else {                                            \
+            typeof((ll)->head) aux = (ll)->head;            \
+            for (size_t i = 1; i <= index; i++)             \
+                aux = aux->next;                            \
+                                                            \
+            (res) = aux->value;                             \
+        }                                                   \
+        res;                                                \
+    })
